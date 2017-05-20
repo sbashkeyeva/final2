5fe9fbe6f4303ed14e2ef7d21770bdf835f95499
sbashkeyeva <sbashkeyeva@gmail.com>
2017-05-20 01:08:18 -0700

    Add files via upload

---
+++ final/final2/final2/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final2
+{
+    public partial class Form1 : Form
+    {
+        Button[,] b = new Button[3, 3];
+        int[] p = new int[11];
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = p[1].ToString();
+            button2.Text = p[2].ToString();
+            button3.Text = p[3].ToString();
+            button4.Text = p[4].ToString();
+            button5.Text = p[5].ToString();
+            button6.Text = p[6].ToString();
+            button7.Text = p[7].ToString();
+            button8.Text = p[8].ToString();
+            button9.Text = p[9].ToString();
+        }
+        int q=0 ;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            p[2]++;
+            if (p[2]==1)
+            {
+                p[10]++;
+                textBox1.Text = p[10].ToString();
+            }
+            else
+            {
+                for (int u = 2; u < p[2]; u++)
+                {
+                    if (p[2] % u == 0)
+                        q++;
+                }
+                if (q == 0)
+                {
+                    p[10]++;
+                    textBox1.Text = p[10].ToString();
+                }
+
+            }
+            
+            
+            button2.Text = p[2].ToString();
+           
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p[1]++;
+            button1.Text = p[1].ToString();
+            if (p[2] % 2 == 0)
+            {
+                p[10]++;
+                textBox1.Text = p[10].ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            p[3]++;
+            button3.Text = p[3].ToString();
+            if (p[2] % 2 == 0)
+            {
+                p[10]++;
+                textBox1.Text = p[10].ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            p[4]++;
+            button4.Text = p[4].ToString();
+            if (p[2] % 2 == 0)
+            {
+                p[10]++;
+                textBox1.Text = p[10].ToString();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            p[5]++;
+            button5.Text = p[5].ToString();
+            if (p[2] % 2 == 0)
+            {
+                p[10]++;
+                textBox1.Text = p[10].ToString();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            p[6]++;
+            button6.Text = p[6].ToString();
+            if (p[2] % 2 == 0 )
+            {
+                p[10]++;
+                textBox1.Text = p[10].ToString();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            p[7]++;
+            button7.Text = p[7].ToString();
+            if (p[2] % 2 == 0)
+            {
+                p[10]++;
+                textBox1.Text = p[10].ToString();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            p[8]++;
+            button8.Text = p[8].ToString();
+            if (p[2] % 2 == 0)
+            {
+                p[10]++;
+                textBox1.Text = p[10].ToString();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            p[9]++;
+            button9.Text = p[9].ToString();
+            if (p[2] % 2 == 0 )
+            {
+                p[10]++;
+                textBox1.Text = p[10].ToString();
+            }
+        }
+    }
+}
---
+++ final/final3/final3/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final3
+{
+    public partial class Form1 : Form
+    {
+        public bool Clicked = false;
+        int i;
+        public Form1()
+        {
+            InitializeComponent();
+            label2.ForeColor = Color.Red;
+
+        }
+        private void labels(int i)
+        {
+            switch (i)
+            {
+
+                case 0:
+                    label1.ForeColor = Color.Red;
+                    label2.ForeColor = Color.Black;
+                    label3.ForeColor = Color.Black;
+                    label4.ForeColor = Color.Black;
+                    label5.ForeColor = Color.Black;
+                    label6.ForeColor = Color.Black;
+                    label7.ForeColor = Color.Black;
+                    label8.ForeColor = Color.Black;
+                    label9.ForeColor = Color.Black;
+                    label10.ForeColor = Color.Black;
+                    label11.ForeColor = Color.Black;
+                    label12.ForeColor = Color.Black;
+                    break;
+                case 1:
+                    label1.ForeColor = Color.Black;
+                    label2.ForeColor = Color.Red;
+                    label3.ForeColor = Color.Black;
+                    label4.ForeColor = Color.Black;
+                    label5.ForeColor = Color.Black;
+                    label6.ForeColor = Color.Black;
+                    label7.ForeColor = Color.Black;
+                    label8.ForeColor = Color.Black;
+                    label9.ForeColor = Color.Black;
+                    label10.ForeColor = Color.Black;
+                    label11.ForeColor = Color.Black;
+                    label12.ForeColor = Color.Black;
+                    break;
+                case 2:
+                    label1.ForeColor = Color.Black;
+                    label2.ForeColor = Color.Black;
+                    label3.ForeColor = Color.Red;
+                    label4.ForeColor = Color.Black;
+                    label5.ForeColor = Color.Black;
+                    label6.ForeColor = Color.Black;
+                    label7.ForeColor = Color.Black;
+                    label8.ForeColor = Color.Black;
+                    label9.ForeColor = Color.Black;
+                    label10.ForeColor = Color.Black;
+                    label11.ForeColor = Color.Black;
+                    label12.ForeColor = Color.Black;
+                    break;
+                case 3:
+                    label1.ForeColor = Color.Black;
+                    label2.ForeColor = Color.Black;
+                    label3.ForeColor = Color.Black;
+                    label4.ForeColor = Color.Red;
+                    label5.ForeColor = Color.Black;
+                    label6.ForeColor = Color.Black;
+                    label7.ForeColor = Color.Black;
+                    label8.ForeColor = Color.Black;
+                    label9.ForeColor = Color.Black;
+                    label10.ForeColor = Color.Black;
+                    label11.ForeColor = Color.Black;
+                    label12.ForeColor = Color.Black;
+                    break;
+                case 4:
+                    label1.ForeColor = Color.Black;
+                    label2.ForeColor = Color.Black;
+                    label3.ForeColor = Color.Black;
+                    label4.ForeColor = Color.Black;
+                    label5.ForeColor = Color.Red;
+                    label6.ForeColor = Color.Black;
+                    label7.ForeColor = Color.Black;
+                    label8.ForeColor = Color.Black;
+                    label9.ForeColor = Color.Black;
+                    label10.ForeColor = Color.Black;
+                    label11.ForeColor = Color.Black;
+                    label12.ForeColor = Color.Black;
+                    break;
+                case 5:
+                    label1.ForeColor = Color.Black;
+                    label2.ForeColor = Color.Black;
+                    label3.ForeColor = Color.Black;
+                    label4.ForeColor = Color.Black;
+                    label5.ForeColor = Color.Black;
+                    label6.ForeColor = Color.Red;
+                    label7.ForeColor = Color.Black;
+                    label8.ForeColor = Color.Black;
+                    label9.ForeColor = Color.Black;
+                    label10.ForeColor = Color.Black;
+                    label11.ForeColor = Color.Black;
+                    label12.ForeColor = Color.Black;
+                    break;
+                case 6:
+                    label1.ForeColor = Color.Black;
+                    label2.ForeColor = Color.Black;
+                    label3.ForeColor = Color.Black;
+                    label4.ForeColor = Color.Black;
+                    label5.ForeColor = Color.Black;
+                    label6.ForeColor = Color.Black;
+                    label7.ForeColor = Color.Red;
+                    label8.ForeColor = Color.Black;
+                    label9.ForeColor = Color.Black;
+                    label10.ForeColor = Color.Black;
+                    label11.ForeColor = Color.Black;
+                    label12.ForeColor = Color.Black;
+                    break;
+                case 7:
+                    label1.ForeColor = Color.Black;
+                    label2.ForeColor = Color.Black;
+                    label3.ForeColor = Color.Black;
+                    label4.ForeColor = Color.Black;
+                    label5.ForeColor = Color.Black;
+                    label6.ForeColor = Color.Black;
+                    label7.ForeColor = Color.Black;
+                    label8.ForeColor = Color.Red;
+                    label9.ForeColor = Color.Black;
+                    label10.ForeColor = Color.Black;
+                    label11.ForeColor = Color.Black;
+                    label12.ForeColor = Color.Black;
+                    break;
+                case 8:
+                    label1.ForeColor = Color.Black;
+                    label2.ForeColor = Color.Black;
+                    label3.ForeColor = Color.Black;
+                    label4.ForeColor = Color.Black;
+                    label5.ForeColor = Color.Black;
+                    label6.ForeColor = Color.Black;
+                    label7.ForeColor = Color.Black;
+                    label8.ForeColor = Color.Black;
+                    label9.ForeColor = Color.Red;
+                    label10.ForeColor = Color.Black;
+                    label11.ForeColor = Color.Black;
+                    label12.ForeColor = Color.Black;
+                    break;
+                case 9:
+                    label1.ForeColor = Color.Black;
+                    label2.ForeColor = Color.Black;
+                    label3.ForeColor = Color.Black;
+                    label4.ForeColor = Color.Black;
+                    label5.ForeColor = Color.Black;
+                    label6.ForeColor = Color.Black;
+                    label7.ForeColor = Color.Black;
+                    label8.ForeColor = Color.Black;
+                    label9.ForeColor = Color.Black;
+                    label10.ForeColor = Color.Red;
+                    label11.ForeColor = Color.Black;
+                    label12.ForeColor = Color.Black;
+                    break;
+                case 10:
+                    label1.ForeColor = Color.Black;
+                    label2.ForeColor = Color.Black;
+                    label3.ForeColor = Color.Black;
+                    label4.ForeColor = Color.Black;
+                    label5.ForeColor = Color.Black;
+                    label6.ForeColor = Color.Black;
+                    label7.ForeColor = Color.Black;
+                    label8.ForeColor = Color.Black;
+                    label9.ForeColor = Color.Black;
+                    label10.ForeColor = Color.Black;
+                    label11.ForeColor = Color.Red;
+                    label12.ForeColor = Color.Black;
+                    break;
+                case 11:
+                    label1.ForeColor = Color.Black;
+                    label2.ForeColor = Color.Black;
+                    label3.ForeColor = Color.Black;
+                    label4.ForeColor = Color.Black;
+                    label5.ForeColor = Color.Black;
+                    label6.ForeColor = Color.Black;
+                    label7.ForeColor = Color.Black;
+                    label8.ForeColor = Color.Black;
+                    label9.ForeColor = Color.Black;
+                    label10.ForeColor = Color.Black;
+                    label11.ForeColor = Color.Black;
+                    label12.ForeColor = Color.Red;
+                    break;
+                   
+
+
+            }
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Button Button = (Button)sender;
+            
+
+        if(i<12)
+            {
+                i++;
+            }
+        else
+            {
+                i = 0;
+            }
+            labels(i);
+            
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (Clicked)
+            {
+                label2.ForeColor = Color.Black;
+                label3.ForeColor = Color.Red;
+            }
+        }
+    }
+}
